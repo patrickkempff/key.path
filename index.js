5981948f74ec2be6958ce4872afec1a44df9d028
Patrick Kempff <patrickkempff@gmail.com>
2017-09-04 14:30:09 +0200

    use exports instead of export

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+'use strict'
 /*!
  * key.path <https://github.com/patrickkempff/key.path>
  *
  * Copyright (c) 2017, Patrick Kempff.
  * Licensed under the MIT License.
  */
- 
+
 const key = path => {
   return typeof path === 'string' ? path.split('.') : path
 }
 
-export const valueForKeyPath = (path, obj) => {
+exports.valueForKeyPath = function(path, obj) {
   return key(path).reduce((k1, k2) => (k1 && k1[k2]) ? k1[k2] : null, obj)
 }
